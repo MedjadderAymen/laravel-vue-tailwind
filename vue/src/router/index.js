918d9d5412bc74a32e5e3f2d9446b97dc7b575ea
MedjadderAymen <medjadder.aimen@gmail.com>
2022-06-27 14:03:00 +0100

    first commit

---
+++ vue/src/router/index.js
@@ -0,0 +1,65 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Surveys from "../views/Surveys.vue";
+import {next} from "lodash/seq";
+import store from "../store";
+import AuthLayout from "../components/AuthLayout.vue";
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    component: DefaultLayout,
+    meta: {requiresAuth: true},
+    children: [
+      {
+        path:'/dashboard', name :'Dashboard', component: Dashboard
+      },
+      {
+        path:'/surveys', name :'Surveys', component: Surveys
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    meta: {isGuest: true},
+    children: [
+      {
+        path:'/login',
+        name:'Login',
+        component: Login
+      },
+      {
+        path:'/register',
+        name:'Register',
+        component: Register
+      }
+    ]
+  },
+];
+
+const router = createRouter({
+  history:createWebHashHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next)=>{
+
+  if(to.meta.requiresAuth && !store.state.user.token ){
+    next({name:'Login'});
+  }else if(store.state.user.token && (to.meta.isGuest)){
+    next({name:'Dashboard'});
+  }else{
+    next();
+  }
+
+})
+
+export default router;
