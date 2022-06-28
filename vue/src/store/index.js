727c0a58583f381a6cec686b6d350069964a1d10
MedjadderAymen <medjadder.aimen@gmail.com>
2022-06-28 18:12:50 +0100

    base layouts & logi/register/logout redirection

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,13 +3,24 @@ import {createStore} from "vuex";
 const store = createStore({
   state: {
     user: {
-      data: {},
-      token: null
+      data: {
+        name: 'Aimen Medjadder',
+        email: 'aimen@gmail.com',
+        imageUrl:
+          'https://mir-s3-cdn-cf.behance.net/user/276/263bbd275526321.5ed29761601cf.jpg',
+      },
+      token: 75
     }
   },
   getters: {},
   actions: {},
-  mutations: {},
+  mutations: {
+    logout: (state) => {
+      console.log('medjadji')
+      state.user.data={};
+      state.user.token=null;
+    }
+  },
   modules: {}
 })
 
